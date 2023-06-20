b16efdd287763c6dc47d4e7232ac1c90241102f2
sayaka.kase <sayakakase1203@outlook.jp>
2023-06-20 13:13:18 +0900

    Like.java LikeService.java を作成

---
+++ src/main/java/models/Like.java
@@ -0,0 +1,36 @@
+package models;
+
+/**
+ * いいねデータのDTOモデル
+ *
+ */
+/*@Table(name = JpaConst.TABLE_LIK)
+@NamedQueries({
+    @NamedQuery(
+            name = JpaConst.Q_LIK_COUNT_ALL,
+            query = JpaConst.Q_LIK_COUNT_ALL_DEF)
+})
+@Getter //全てのクラスフィールドについてgetterを自動生成する(Lombok)
+@Setter //全てのクラスフィールドについてsetterを自動生成する(Lombok)
+@NoArgsConstructor //引数なしコンストラクタを自動生成する(Lombok)
+@AllArgsConstructor //全てのクラスフィールドを引数にもつ引数ありコンストラクタを自動生成する(Lombok)
+@Entity
+
+public class Like {
+    /**
+     * id
+     */
+   /* @Id
+    @Column(name = JpaConst.LIK_COL_ID)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    /**
+     * いいねを登録した従業員
+     */
+    /*@ManyToOne
+    @JoinColumn(name = JpaConst.REP_COL_EMP)
+   private Employee employee;
+
+
+}*/
---
+++ src/main/java/services/LikeService.java
@@ -0,0 +1,5 @@
+package services;
+
+public class LikeService extends ServiceBase {
+
+}
